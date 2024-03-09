3318900fbb5d31d0d48b0a4696e6ecfe0e2a0502
Josehpequeno <hicarojbs21@gmail.com>
2024-03-08 23:59:59 -0300

    mudando de página

--- go.mod
+++ go.mod
@@ -2,14 +2,22 @@ module lumus
 
 go 1.18
 
+require github.com/creack/pty v1.1.21
+
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dslipak/pdf v0.0.2 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/ledongthuc/pdf v0.0.0-20240201131950-da5b75280b06 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -18,8 +26,10 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pdfcpu/pdfcpu v0.7.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sirupsen/logrus v1.5.0 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/unidoc/pkcs7 v0.2.0 // indirect
@@ -29,9 +39,10 @@ require (
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,26 +4,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/gdamore/tcell/v2"
-	"github.com/unidoc/unipdf/v3/extractor"
-	unipdf "github.com/unidoc/unipdf/v3/model"
+	"github.com/inancgumus/screen"
+	"github.com/ledongthuc/pdf"
 )
 
 type model struct {
-	Files         []os.FileInfo
-	CurrentIdx    int
-	Content       string
-	FontSize      int
-	FontStyle     tcell.Style
-	DefaultStyle  tcell.Style
-	SelectedStyle tcell.Style
-	Loading       bool
-	CurrentPage   int
-	TotalPages    int
+	Files       []os.FileInfo
+	CurrentIdx  int
+	Content     string
+	FontSize    int
+	Loading     bool
+	CurrentPage int
+	TotalPages  int
+	ReadingMode bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -57,22 +56,16 @@ func initialModel() model {
 		os.Exit(1)
 	}
 
-	style := tcell.StyleDefault.
-		Foreground(tcell.ColorWhite).
-		Background(tcell.ColorBlack)
-
 	return model{
-		Files:         files,
-		CurrentIdx:    0,
-		Content:       "Select a file to view its content",
-		FontSize:      1,
-		FontStyle:     style,
-		DefaultStyle:  style,
-		SelectedStyle: style.Foreground(tcell.ColorBlue),
+		Files:       files,
+		CurrentIdx:  0,
+		Content:     "Select a file to view its content",
+		FontSize:    12,
+		ReadingMode: false,
+		CurrentPage: 1,
 	}
 }
 
-// funtions of model
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -88,13 +81,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) handleKeyMsg(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	switch msg.String() {
 	case "q":
-		return m, tea.Quit
+		return m.handleQuitKey()
 	case "enter":
 		return m.handleEnterKey()
-	case "up", "k":
+	case "up", "w":
 		return m.handleUpKey()
-	case "down", "j":
+	case "down", "s":
 		return m.handleDownKey()
+	case "right", "d":
+		return m.handleRightKey()
+	case "left", "a":
+		return m.handleLeftKey()
 	case "+":
 		return m.handlePlusKey()
 	case "-":
@@ -111,6 +108,7 @@ func (m model) handleLoadContentMsg(msg LoadContentMsg) (tea.Model, tea.Cmd) {
 	}
 	m.Content = content
 	m.TotalPages = totalPages
+	m.ReadingMode = true
 	return m, tea.Tick(time.Second/2, func(t time.Time) tea.Msg {
 		return LoadingDone
 	})
@@ -126,9 +124,18 @@ func (m model) handleMsgType(msg MsgType) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+func (m model) handleQuitKey() (tea.Model, tea.Cmd) {
+	if m.ReadingMode {
+		m.ReadingMode = false
+		screen.Clear()
+		return m, nil
+	}
+	return m, tea.Quit
+}
+
 func (m model) handleEnterKey() (tea.Model, tea.Cmd) {
-	// Lógica para lidar com a tecla "enter"
 	m.Loading = true
+	screen.Clear()
 	return m, func() tea.Msg {
 		return LoadContentMsg{FileName: m.Files[m.CurrentIdx].Name(), Page: m.CurrentPage}
 	}
@@ -152,12 +159,41 @@ func (m model) handleDownKey() (tea.Model, tea.Cmd) {
 
 func (m model) handlePlusKey() (tea.Model, tea.Cmd) {
 	m.FontSize++
+	// screen.Clear()
+	setFontSize(m.FontSize * 2)
 	return m, nil
 }
 
 func (m model) handleMinusKey() (tea.Model, tea.Cmd) {
 	if m.FontSize > 1 {
 		m.FontSize--
+		setFontSize(m.FontSize * 2)
+	}
+	return m, nil
+}
+
+func setFontSize(size int) {
+	cmd := exec.Command("resize", "-s", strconv.Itoa(size), strconv.Itoa(size*2))
+	cmd.Run()
+	time.Sleep(time.Millisecond * 100) // Give the terminal time to react
+}
+
+func (m model) handleRightKey() (tea.Model, tea.Cmd) {
+	if m.ReadingMode && m.CurrentPage < m.TotalPages {
+		m.CurrentPage++
+		return m, func() tea.Msg {
+			return LoadContentMsg{FileName: m.Files[m.CurrentIdx].Name(), Page: m.CurrentPage}
+		}
+	}
+	return m, nil
+}
+
+func (m model) handleLeftKey() (tea.Model, tea.Cmd) {
+	if m.ReadingMode && m.CurrentPage > 1 {
+		m.CurrentPage--
+		return m, func() tea.Msg {
+			return LoadContentMsg{FileName: m.Files[m.CurrentIdx].Name(), Page: m.CurrentPage}
+		}
 	}
 	return m, nil
 }
@@ -165,28 +201,40 @@ func (m model) handleMinusKey() (tea.Model, tea.Cmd) {
 func (m model) handleGoToPage() (tea.Model, tea.Cmd) {
 	page, err := strconv.Atoi(m.Content)
 	if err != nil || page < 1 || page > m.TotalPages {
-		// Se a página for inválida, retorne o modelo atual sem comandos
 		return m, nil
 	}
 
 	m.CurrentPage = page
 	m.Loading = true
 
-	// Retorna o modelo com um comando para carregar o conteúdo da página especificada
 	return m, func() tea.Msg {
 		return LoadContentMsg{FileName: m.Files[m.CurrentIdx].Name(), Page: m.CurrentPage}
 	}
 }
 
 func (m model) handleLoadingDone() (tea.Model, tea.Cmd) {
-	// Define Loading como false para indicar que o carregamento foi concluído
 	m.Loading = false
-
-	// Retorna o modelo atual sem comandos adicionais
 	return m, nil
 }
 
 func (m model) View() string {
+	if m.Loading {
+		return "\nLoading..."
+	}
+
+	if m.ReadingMode {
+		termWidth, _ := screen.Size()
+
+		contentLines := divideTextIntoLines(m.Content, termWidth)
+
+		contentText := strings.Join(contentLines, "\n")
+
+		pageInfo := fmt.Sprintf("\nContent (Font Size %d, Page %d/%d):\n", m.FontSize, m.CurrentPage, m.TotalPages)
+
+		return contentText + pageInfo
+
+	}
+
 	s := "Files:\n"
 	for i, file := range m.Files {
 		if i == m.CurrentIdx {
@@ -196,14 +244,28 @@ func (m model) View() string {
 		}
 	}
 
-	if m.Loading {
-		s += "\nLoading..."
-	} else {
-		s += fmt.Sprintf("\nContent (Font Size %d, Page %d/%d):\n", m.FontSize, m.CurrentPage, m.TotalPages)
-		s += m.Content
+	return s
+}
+
+func divideTextIntoLines(text string, maxWidth int) []string {
+	var lines []string
+	words := strings.Fields(text)
+	currentLine := ""
+
+	for _, word := range words {
+		if len(currentLine)+len(word) < maxWidth {
+			currentLine += word + " "
+		} else {
+			lines = append(lines, currentLine)
+			currentLine = word + " "
+		}
 	}
 
-	return s
+	if currentLine != "" {
+		lines = append(lines, currentLine)
+	}
+
+	return lines
 }
 
 type LoadContentMsg struct {
@@ -212,33 +274,19 @@ type LoadContentMsg struct {
 }
 
 func readPDFFile(fileName string, pageNum int) (string, int, error) {
-	f, err := os.Open(fileName)
+	f, r, err := pdf.Open(fileName)
 	if err != nil {
 		return "", 0, err
 	}
-	defer f.Close()
+	defer func() {
+		_ = f.Close()
+	}()
 
-	pdfReader, err := unipdf.NewPdfReader(f)
-	if err != nil {
-		return "", 0, err
-	}
+	totalPages := r.NumPage()
 
-	totalPages, err := pdfReader.GetNumPages()
-	if err != nil {
-		return "", 0, err
-	}
+	page := r.Page(pageNum)
 
-	page, err := pdfReader.GetPage(pageNum)
-	if err != nil {
-		return "", 0, err
-	}
-
-	ex, err := extractor.New(page)
-	if err != nil {
-		return "", 0, err
-	}
-
-	pageContent, err := ex.ExtractText()
+	pageContent, err := page.GetPlainText(nil)
 	if err != nil {
 		return "", 0, err
 	}
